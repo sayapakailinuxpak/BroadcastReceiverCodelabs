34a79819636c90fd911d437b6c0c05bd3de9b34b
sayapakailinuxpak <sayapakailinuxpak@gmail.com>
2020-03-30 07:25:17 +0700

    Dynamic Registering receiver, IntentFilter, and avoid memory leaks

---
+++ app/src/main/java/com/eldisprojectcodelabs/broadcastreceivercodelabs/MainActivity.java
@@ -0,0 +1,33 @@
+package com.eldisprojectcodelabs.broadcastreceivercodelabs;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private CustomReceiver customReceiver = new CustomReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Intent filter specifies the types of intents a component can receive
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        //register and unregister the receiver using activity context
+        MainActivity.this.registerReceiver(customReceiver, intentFilter);
+    }
+
+
+    //avoid memory leaks
+    @Override
+    protected void onDestroy() {
+        MainActivity.this.unregisterReceiver(customReceiver);
+        super.onDestroy();
+    }
+}
